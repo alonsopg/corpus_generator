78c12d7496ec385811a2eb0d0eff929ce6f5a75f
Alonso <alonsop@ciencias.unam.mx>
2016-03-07 02:36:06 -0600

    Initial commit

---
+++ corpus_creator_v1.py
@@ -0,0 +1,24 @@
+import glob, os, csv, argparse
+
+def retrive(directory):
+    for filename in sorted(glob.glob(os.path.join(directory, '*.txt'))):
+        with open(filename, 'r') as f:
+            important_stuff = f.read().splitlines()
+            oneline = [''.join(important_stuff)]
+            yield filename.split('/')[-1] + ', ' +str(oneline).strip('[]"')
+
+NAME='ef_ngrams'
+prefix='1grams'
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser(NAME)
+
+    p.add_argument("DIR",default=None,
+        action="store", help="Directory with corpus with json")
+    opts = p.parse_args()
+    test = tuple(retrive(opts.DIR))
+    with open('/Users/user/Downloads/corpus.csv','w') as out:
+        csv_out=csv.writer(out, delimiter='|')
+        csv_out.writerow(['id','content'])
+        for row in test:
+            csv_out.writerow(row.split(', ', 1))
