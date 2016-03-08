663687398be8a808692dbebfabd0c230d5c92b64
Alonso <alonsop@ciencias.unam.mx>
2016-03-08 14:46:33 -0600

    Update corpus_creator_v1.py

--- corpus_creator_v1.py
+++ corpus_creator_v1.py
@@ -10,10 +10,9 @@ def retrive(directory):
             oneline = [''.join(important_stuff)]
             yield filename.split('/')[-1] + ', ' +str(oneline).strip('[]"')
 
-
 def corpus_writer(directory2):
-    test = tuple(retrive(opts.DIR))
-    with open('new_corpus.csv','w') as out:
+    test = tuple(retrive(opts.input))
+    with open(opts.output,'w') as out:
         csv_out=csv.writer(out, delimiter='|')
         csv_out.writerow(['id','content'])
         for row in test:
@@ -26,11 +25,10 @@ prefix='corpus'
 if __name__ == "__main__":
     p = argparse.ArgumentParser(NAME)
 
-    p.add_argument("DIR", default=None,
-                   action="store", help="Inpunt corpus files")
+    p.add_argument('-input_directory:', '--input', default=sys.stdin)
 
-    p.add_argument('-o', '--output', default=sys.stdout)
+    p.add_argument('-output_corpus:', '--output', default=sys.stdout)
 
     opts = p.parse_args()
-    retrive(opts.DIR)
+    retrive(opts.input)
     corpus_writer(opts.output)
